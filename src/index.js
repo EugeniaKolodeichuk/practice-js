be3814b1b0b97972d0144823ed47b95d2d688677
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-04-08 01:13:26 +0300

    add the main functionality of the game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
+let score = 0;
+let level = 1;
+let levelThreshold = [10, 20, 30, 40, 50];
 
+// Get the elements
+const clickBtn = document.querySelector('#clickBtn');
+const scoreText = document.querySelector('#score');
+const levelText = document.querySelector('#level p');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const registrationBtn = document.querySelector('button');
+const registerForm = document.querySelector('form');
+
+// Add a click event listener to the button and the form
+clickBtn.addEventListener('click', function () {
+  score++;
+  scoreText.innerHTML = `Score: ${score}`;
+  checkLevel();
+
+  if (score === 5) {
+  }
+});
+
+registerForm?.addEventListener('submit', register);
+
+function checkLevel() {
+  if (score >= levelThreshold[level - 1]) {
+    level++;
+    levelText.innerHTML = `Level: ${level}`;
+  }
+}
+
+function register(e) {
+  e.preventDefault();
+  const name = nameInput.value;
+  const email = emailInput.value;
+  console.log(`Name: ${name}, Email: ${email}`);
+  nameInput.value = '';
+  emailInput.value = '';
+  window.location = 'levels.html';
+}
