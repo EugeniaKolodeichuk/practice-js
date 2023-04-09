863c321d4474ceb24522fb07eab18ea1766b5f7e
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-04-09 22:06:32 +0300

    add required form fields and saving to LS

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
-let score = 0;
-let level = 1;
-let levelThreshold = [10, 20, 30, 40, 50];
-
-// Get the elements
-const clickBtn = document.querySelector('#clickBtn');
-const scoreText = document.querySelector('#score');
-const levelText = document.querySelector('#level p');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
-const registrationBtn = document.querySelector('button');
 const registerForm = document.querySelector('form');
 
-// Add a click event listener to the button and the form
-clickBtn.addEventListener('click', function () {
-  score++;
-  scoreText.innerHTML = `Score: ${score}`;
-  checkLevel();
-
-  if (score === 5) {
-  }
-});
-
-registerForm?.addEventListener('submit', register);
-
-function checkLevel() {
-  if (score >= levelThreshold[level - 1]) {
-    level++;
-    levelText.innerHTML = `Level: ${level}`;
-  }
-}
+registerForm.addEventListener('submit', register);
 
 function register(e) {
   e.preventDefault();
@@ -37,5 +11,6 @@ function register(e) {
   console.log(`Name: ${name}, Email: ${email}`);
   nameInput.value = '';
   emailInput.value = '';
+  localStorage.setItem('userData', JSON.stringify({ name: name, email: email }));
   window.location = 'levels.html';
 }
