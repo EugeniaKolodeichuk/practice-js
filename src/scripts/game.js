5ad4652f3d0c3e968f1fd84291bd2fdb4e6371cf
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-04-12 00:52:43 +0300

    add styles

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,7 +1,6 @@
 let score = 0;
 let level = 1;
 let levelThreshold = [5, 10, 15, 20, 25];
-let userName = 'User';
 
 // Get the elements
 const clickBtn = document.getElementById('clickBtn');
@@ -11,17 +10,17 @@ const levelText = document.querySelector('#level p');
 const greeting = document.getElementById('greeting');
 const clicker = document.getElementById('clicker');
 const heroImg = document.getElementById('level-image');
-
 const modalContainer = document.getElementById('modal-container');
 const modalMessage = document.getElementById('modal-message');
 const modalCloseBtn = document.getElementById('modal-close-btn');
+const congratulation = document.getElementById('congratulation');
+const congratulationText = document.querySelector('#congratulation p');
+const mainPage = document.getElementById('game');
 
 const savedData = JSON.parse(localStorage.getItem('userData'));
-console.log('userData', savedData);
-if (savedData?.name) {
-  userName = savedData.name;
-}
-greeting.innerText = `Hi, ${userName}`;
+const userName = savedData.name;
+
+greeting.innerText = `Hi, ${userName}!`;
 healthText.innerText = `Health: ${levelThreshold[level - 1] - score}`;
 
 // Add a click event listener to the button and the form
@@ -47,11 +46,10 @@ function checkLevel() {
     heroImg.src = './assets/level' + level + '.png';
     showModal();
   } else if (score === levelThreshold[levelThreshold.length - 1]) {
-    levelText.style.display = 'none';
-    healthText.style.display = 'none';
-    scoreText.innerText = `You win! Total clicks ${score}`;
-    heroImg.src = './assets/win' + '.png';
     heroImg.removeEventListener('click', increaseScore);
+    game.style.display = 'none';
+    congratulation.style.display = 'flex';
+    congratulationText.innerText = `You win! Total clicks ${score}!`;
     return;
   }
 }
